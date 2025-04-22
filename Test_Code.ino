07b83021ad596fe72f11499bed359e149f3ecf4b
GerardSanchezSanchez <gerard.sanchez.sanchez@uvic.cat>
2025-04-22 17:16:11 +0200

    Test Code updates and minor fixes
    Added support for Servomotors in the test code.
    
    Must decide when will servomotors be active for better use.
    
    Implementatios require hardware to be added to the project (capacitive sensor and limit stop sensor) but can be tested without.
    
    Electronics Leader must confirm addition of previously mentioned components.

--- Test_Code.ino
+++ Test_Code.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_INA219.h>
+#include <Servo.h>
 
 bool active = false;
 int current_ina, i;
@@ -7,23 +8,27 @@ int current_ina, i;
 int p_dir_1 = 10; // DIRECTION 1
 int p_dir_2 = 11; // DIRECTION 2
 int p_spd = 3;    // PWM SPEED
-int p_clg = 2;    // CLOGGING ANALOG PIN
 int p_ppr = 21;   // PAPER DETECTION INTERRUPT
-//int p_vtg = pin; // VOLTAGE CONTROL PIN
+//int p_srv_1 = pin;
+//int p_srv_2 = pin;
+//int p_vtg = pin;  // VOLTAGE CONTROL PIN (STOP DRIVER FULLY IF DOOR OPENS)
 //int p_drc = pin;  // DOOR CLOSE PIN
 
+Servo servo_1;    // DECLARE SERVOS
+Servo servo_2;
 Adafruit_INA219 ina219;
 
 void setup() {
   Serial.begin(115200);
 
   ina219.begin();
+  //servo_1.attach(p_srv_1);  // ATTACH SERVOS TO CONTROL PINS
+  //servo_2.attach(p_srv_2);
 
-  pinMode(p_clg, INPUT);  // CLOGGING PIN
-  pinMode(p_ppr, INPUT);  // CLOGGING PIN
-  pinMode(p_spd, OUTPUT); // VELOCITAT
-  pinMode(p_dir_1, OUTPUT); // DIRECCIÓ
-  pinMode(p_dir_2, OUTPUT);  //DIRECCIÓ
+  pinMode(p_ppr, INPUT);  // PAPER DETECTION PIN
+  pinMode(p_spd, OUTPUT); // SPEED
+  pinMode(p_dir_1, OUTPUT);  // DIRECTION 1
+  pinMode(p_dir_2, OUTPUT);  // DIRECTION 2
 
   TCCR3A = ( TCCR3A & B11111001 | B00000001 );    // TIMER MAIN MOTOR 
   TCCR3B = ( TCCR3B & B11111010 | B00000010 );
@@ -32,16 +37,16 @@ void setup() {
   TCCR1B = ( TCCR1B & B11101000 | B00001000 );    // PRESCALER OFF
   OCR1A = 62500;
 
-  EIMSK |= B00000001;   // ENABLE INT1 PIN 21 INTERRUPT
+  EIMSK |= B00000001;   // ENABLE INT1 PIN 21 INTERRUPT (PAPER DETECTION)
   EICRA |= B00000010;   // FALLING EDGE INTERRUPT
 
   digitalWrite(p_spd, LOW); // check
 }
 
-isr(INT0_vect){           // LACK OF PAPER DETECTION
+ISR(INT0_vect){           // LACK OF PAPER DETECTION (PIN 21)
   TCCR1B = ( TCCR1B & B11111101 | B00000101 );  // TIMER ON MAX PRESCALER
 }
-isr(TIMER1_COMPA_vect){   // 4 SECONDS OVERWORK
+ISR(TIMER1_COMPA_vect){   // 4 SECONDS OVERWORK
   TCCR1B = ( TCCR1B & B11111000 | B00000000 );  // TIMER OFF PRESCALER 0
   TCNT1 = 0;
   active = false;
@@ -54,19 +59,26 @@ void loop() {
     }
 
     if(active){   // NORMAL WORKING STATUS
-      digitalWrite(p_dir_1, LOW); // DIRECTION
-      digitalWrite(p_dir_2, HIGH); // DIRECTION
-      analogWrite(p_spd, 200); // PWM 200
+      //servo_1.write(angle);       // DECLOGGING ON POSITION
+      //servo_2.write(angle);
+      
+      digitalWrite(p_dir_1, LOW);   // DIRECTION
+      digitalWrite(p_dir_2, HIGH);  // DIRECTION
+      analogWrite(p_spd, 200);      // PWM 200
     }
 
     current_ina = ina219.getCurrent_mA();
     Serial.println(current_ina);
-    if(current_ina > 1000){
-      digitalWrite(p_dir_1, LOW); // STOP
+    if(current_ina > 1000){         // ANTI-CLOGGING 
+
+      //servo_1.write(angle);       // DECLOGGING OFF POSITION
+      //servo_2.write(angle);
+
+      digitalWrite(p_dir_1, LOW);   // STOP MOTOR
       digitalWrite(p_dir_2, LOW);
       delay(500);
       
-      digitalWrite(p_dir_1, HIGH); // SWAP DIRECTIONS
+      digitalWrite(p_dir_1, HIGH);  // SWAP DIRECTIONS
       digitalWrite(p_dir_2, LOW);
       for(i=50;i<101;i++){
         analogWrite(p_spd, i);
@@ -74,7 +86,7 @@ void loop() {
       }
       delay(1000);
       
-      digitalWrite(p_dir_1, LOW);
+      digitalWrite(p_dir_1, LOW);   // RESTART WORKING STATUS
       digitalWrite(p_dir_2, HIGH);
       analogWrite(p_spd,100);
       delay(500);
